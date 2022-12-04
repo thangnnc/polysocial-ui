04296f5764655e01d04b15ce54b483485d180757
Trần Mậu Phi <phitmps16509@fpt.edu.vn>
2022-12-05 00:49:45 +0700

    update

--- src/sections/@dashboard/user/UserListToolbar.js
+++ src/sections/@dashboard/user/UserListToolbar.js
@@ -1,27 +1,35 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 // @mui
-import { styled, alpha } from '@mui/material/styles';
-import { Toolbar, Tooltip, IconButton, Typography, OutlinedInput, InputAdornment } from '@mui/material';
+import { styled, alpha } from "@mui/material/styles";
+import {
+  Toolbar,
+  Tooltip,
+  IconButton,
+  Typography,
+  OutlinedInput,
+  InputAdornment,
+} from "@mui/material";
 // component
-import Iconify from '../../../components/iconify';
-import { Fragment } from 'react';
+import Iconify from "../../../components/iconify";
+import { Fragment } from "react";
 
 // ----------------------------------------------------------------------
 
 const StyledRoot = styled(Toolbar)(({ theme }) => ({
   height: 96,
-  display: 'flex',
-  justifyContent: 'space-between',
+  display: "flex",
+  justifyContent: "space-between",
   padding: theme.spacing(0, 1, 0, 3),
 }));
 
 const StyledSearch = styled(OutlinedInput)(({ theme }) => ({
-  width: 320,
-  transition: theme.transitions.create(['box-shadow', 'width'], {
+  width: 360,
+  height: 50,
+  transition: theme.transitions.create(["box-shadow", "width"], {
     easing: theme.transitions.easing.easeInOut,
     duration: theme.transitions.duration.shorter,
   }),
-  '& fieldset': {
+  "& fieldset": {
     borderWidth: `1px !important`,
     borderColor: `${alpha(theme.palette.grey[500], 0.32)} !important`,
   },
@@ -35,13 +43,17 @@ UserListToolbar.propTypes = {
   onFilterName: PropTypes.func,
 };
 
-export default function UserListToolbar({ numSelected, filterName, onFilterName }) {
+export default function UserListToolbar({
+  numSelected,
+  filterName,
+  onFilterName,
+}) {
   return (
     <StyledRoot
       sx={{
         ...(numSelected > 0 && {
-          color: 'primary.main',
-          bgcolor: 'primary.lighter',
+          color: "primary.main",
+          bgcolor: "primary.lighter",
         }),
       }}
     >
@@ -53,10 +65,13 @@ export default function UserListToolbar({ numSelected, filterName, onFilterName 
         <StyledSearch
           value={filterName}
           onChange={onFilterName}
-          placeholder="Tìm kiếm......"
+          placeholder="Tìm kiếm tên người dùng"
           startAdornment={
             <InputAdornment position="start">
-              <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled', width: 20, height: 20 }} />
+              <Iconify
+                icon="eva:search-fill"
+                sx={{ color: "text.disabled", width: 20, height: 20 }}
+              />
             </InputAdornment>
           }
         />
@@ -68,7 +83,9 @@ export default function UserListToolbar({ numSelected, filterName, onFilterName 
             <Iconify icon="eva:trash-2-fill" />
           </IconButton>
         </Tooltip>
-      ) : (<Fragment />)}
+      ) : (
+        <Fragment />
+      )}
     </StyledRoot>
   );
 }
